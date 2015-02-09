c7cc31c68a9104edd58b6d3934916e7b72e20b9f
kuroyam <kuroyam1109@gmail.com>
2015-02-09 23:26:55 +0900

    chapter5が終わった

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   match '/contact', to: 'static_pages#contact', via: 'get'
 
+  match '/signup', to: 'users#new', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
